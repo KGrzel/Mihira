bbe1e1f761776b56fc1477b4c3de8452a5cd75aa
Kamil Grzelinski <kamilgrzelinski@sastrans.pl>
2024-02-09 13:30:56 +0100

    Updates

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 
 export const riotApiKey = process.env.riotApiKey
-export const TOKEN = process.env.TOKEN
+export const TOKEN = process.env.DISCORD_TOKEN
 export const CLIENT_ID = process.env.CLIENT_ID
 export const GUILD_ID = process.env.GUILD_ID
